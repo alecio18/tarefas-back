6f06023793a3945ac7cc573d3d3794c67bbfed83
Alécio <alecio18@hotmail.com>
2020-04-07 03:43:53 -0300

    alterado endereco do mongo

--- src/config/database.js
+++ src/config/database.js
@@ -7,4 +7,4 @@ const mongoose = require('mongoose')
 /* remover advertencia usando a promise do node */
  mongoose.Promise = global.Promise
 
- module.exports = mongoose.connect('mongodb://localhost/tarefas')
+ module.exports = mongoose.connect("mongodb+srv://alecio:alecio10@cluster0-vah56.gcp.mongodb.net/test?retryWrites=true&w=majority")
