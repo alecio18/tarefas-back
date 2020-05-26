df9492978bc017e61d05a777db7591b115b84d16
Alécio <alecio18@hotmail.com>
2020-05-26 11:42:31 -0300

    add dataConclusao

--- src/api/tarefas/tarefas.js
+++ src/api/tarefas/tarefas.js
@@ -5,6 +5,7 @@ const tarefasSchema = new mongoose.Schema({
     descricao: { type: String, required: true },
     completo: { type: Boolean, require: true, default: false },
     dataCriacao: { type: Date, default: Date.now },
+    dataConclusao: { type: Date, require: false },
     tipo : { type: String, required: false, default: '' }
 })
 
